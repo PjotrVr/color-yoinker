d30f91391427636d7f6efbcf4aacf33d2fd7e0b0
PjotrVr <petar.stika12345@gmail.com>
2021-11-30 04:46:26 +0100

    Added like button

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-buttons = document.querySelectorAll("button");
+buttons = document.querySelectorAll(".main");
+buttons_like = document.querySelectorAll(".like");
 buttons_len = buttons.length;
+buttons_like_values = Array(buttons_len);
+
+for(let i=0; i < buttons_len; i++){
+    buttons_like_values[i] = 0;
+}
 
 for(let i=0; i < buttons_len; i++){
     buttons[i].addEventListener("click", function(){
         navigator.clipboard.writeText(buttons[i].innerText);
         alert("Color " + buttons[i].innerText + " has been added to clipboard!");
-    })
+    });
+
+    buttons_like[i].addEventListener("click", function(){
+        if(buttons_like_values[i] === 0){
+            buttons_like[i].innerText = Number(buttons_like[i].innerText.replace(" 👍", "")) + 1 + " 👍";
+            buttons_like[i].style.backgroundColor = "blue";
+            buttons_like_values[i] = 1;
+        }
+        else if(buttons_like_values[i] === 1){
+            buttons_like[i].innerText = Number(buttons_like[i].innerText.replace(" 👍", "")) - 1 + " 👍";
+            buttons_like[i].style.backgroundColor = "yellow";
+            buttons_like_values[i] = 0;
+        }
+    });
 }
 
 inp = document.querySelector("input");
@@ -20,6 +39,7 @@ function fullReset(){
     for(let i=0; i < buttons_len; i++){
         reset(i);
         buttons[i].style.display = "inline";
+        buttons_like[i].style.display = "inline";
     }
 }
 
@@ -33,13 +53,14 @@ function updateInput () {
             else{
                 reset(i);
                 buttons[i].style.display = "none";
+                buttons_like[i].style.display = "none";
+            
             }
         }   
     }
 
     else {
         fullReset();
-        location.reload();
+        
     }
-};
-
+}
